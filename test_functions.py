b90d3409c186e4afd3d8764a9e7522d8123932f2
Nithin Prince John <nithinpj09@gmail.com>
2023-06-09 12:36:40 +0530

    Update test_functions.py

--- test_functions.py
+++ test_functions.py
@@ -1,4 +1,3 @@
-
 from functions import add, subtract, multiply
 from functions import convert_fahrenheit_to_celsius as f2c
 import pytest
@@ -7,11 +6,12 @@ def test_add():
     assert add(2, 3) == 5
     assert add('space', 'ship') == 'spaceship'
 
-# uncomment the following test in step 5
 def test_subtract():
     assert subtract(2, 3) == -1
+    
+def test_multiply():
+    assert subtract(2, 3) == 6
 
-# uncomment the following test in step 11
 def test_convert_fahrenheit_to_celsius():
     assert f2c(32) == 0
     assert f2c(122) == pytest.approx(50)
